6647d1bdb6c57204041ae4160cbdb77602e464a0
Manikandan81480 <mani81480@gmail.com>
2017-01-01 02:51:08 -0800

    Update and rename README.md to stoint.java

---
+++ stoint.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class stringtointegerconvert
+{
+public static void main(String...args)
+{
+Scanner x=new Scanner(System.in);
+String s=x.nextLine();
+int a=Integer.parseInt(s);
+System.out.println(a);
+}
+}
